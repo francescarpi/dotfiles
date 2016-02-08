3af6890e0f4ad6805e4042553844b49155977e2a
Francesc Arpi <francesc.arpi@gmail.com>
2016-02-08 20:20:17 +0100

    Rc lua

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -389,11 +389,7 @@ awful.rules.rules = {
                      raise = true,
                      keys = clientkeys,
                      buttons = clientbuttons } },
-    { rule = { class = "MPlayer" },
-      properties = { floating = true } },
-    { rule = { class = "pinentry" },
-      properties = { floating = true } },
-    { rule = { class = "gimp" },
+    { rule = { class = "galculator" },
       properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
