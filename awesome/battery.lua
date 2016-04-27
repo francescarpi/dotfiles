2aa0561688a5f151683d54f25bc05ed91a511ba6
Francesc Arpi <francesc.arpi@gmail.com>
2016-04-27 20:48:22 +0200

    New icon teheme

--- awesome/battery.lua
+++ awesome/battery.lua
@@ -20,16 +20,20 @@ function update_battery()
 
     text:set_text(" " .. percent .. "% ")
 
-    if percent >= 0 and percent <= 10 then
+    if percent >= 0 and percent <= 5 then
         icon:set_image(os.getenv("HOME") .. "/.dotfiles/awesome/icons/battery-caution.png")
-    elseif percent > 10 and percent <= 25 then
+    elseif percent > 5 and percent <= 10 then
         icon:set_image(os.getenv("HOME") .. "/.dotfiles/awesome/icons/battery-low.png")
-    elseif percent > 25 and percent <= 75 then
+    elseif percent > 10 and percent <= 75 then
         icon:set_image(os.getenv("HOME") .. "/.dotfiles/awesome/icons/battery-good.png")
-    elseif percent> 75 then
+    elseif percent > 75 then
         icon:set_image(os.getenv("HOME") .. "/.dotfiles/awesome/icons/battery-full.png")
     end
 
 end
 
 update_battery()
+
+mytimer = timer({ timeout = 60 })
+mytimer:connect_signal("timeout", function () update_battery() end)
+mytimer:start()
