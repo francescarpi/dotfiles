e6551a733be1b689998e1d97927d2f7d66dd6df0
Francesc Arpi <francesc.arpi@gmail.com>
2016-06-08 22:01:15 +0200

    status

--- i3/status.py
+++ i3/status.py
@@ -9,5 +9,15 @@ status = Status(standalone=True)
 
 status.register("clock", format="%d/%m/%Y %H:%M",)
 status.register("load")
-status.register("pulseaudio", format="♪{volume}% {muted}",)
+status.register("pulseaudio", format="AUDIO: {volume}% {muted}",)
+status.register("battery", format=("{status}/{consumption:.2f}W {percentage:.2f}% "
+                "[{percentage_design:.2f}%] {remaining:%E%hh:%Mm}"),
+                path="/sys/class/power_supply/BAT0/uevent",
+                alert=True, alert_percentage=5, status={"DIS": "↓", "CHR": "↑", "FULL": "=",})
+
+status.register("network", interface="wlp4s0",
+                format_up="{essid} {quality:.0f}%",)
+
+status.register("backlight", format="BRILLO: {percentage:.0f}%",
+                backlight="intel_backlight")
 status.run()
