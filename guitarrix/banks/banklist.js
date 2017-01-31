734800325d8171dfd03bce7199769a6c9c29bd3f
Francesc Arpi <francesc.arpi@gmail.com>
2017-01-31 23:14:08 +0100

    Backup

--- guitarrix/banks/banklist.js
+++ guitarrix/banks/banklist.js
@@ -1,4 +1,6 @@
 [
+  ["nuclear", "nuclear.gx", 1, 0, [1, 2], 1485190760],
+  ["mkenny_17tone", "mkenny_17tone.gx", 1, 0, [1, 2], 1485190737],
   ["Cesc", "Cesc.gx", 1, 0, [1, 2], 1484998849],
-  ["Scratchpad", "scratchpad.gx", 0, 0, [1, 2], 1484998849]
+  ["Scratchpad", "scratchpad.gx", 0, 0, [1, 2], 1484998889]
 ]
