6e020b9da18d574f80ec0e27c17278108f16587f
Francesc Arpi <francesc.arpi@gmail.com>
2017-05-28 20:07:49 +0200

    Backup

--- awesome/spotify/spotify.lua
+++ awesome/spotify/spotify.lua
@@ -3,25 +3,18 @@ local awful = require("awful")
 local gfs = require("gears.filesystem")
 local cfg_path = gfs.get_configuration_dir()
 
-local text = wibox.widget.textbox()
-
 spotify_widget = wibox.layout {
     layout = wibox.layout.fixed.horizontal,
     {
         widget = wibox.container.margin,
         top = 4,
         left = 5,
+        right = 5,
         {
             widget = wibox.widget.imagebox,
             image = cfg_path .. "spotify/spotify.png",
             forced_width = 13
         }
-    },
-    {
-        widget = wibox.container.margin,
-        left = 5,
-        right = 10,
-        text
     }
 }
 
@@ -30,15 +23,3 @@ spotify_widget:buttons(awful.util.table.join(
         awful.util.spawn("dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
     end)
 ))
-
-awful.widget.watch(
-    'pidof -s spotify', 1,
-    function(widget, stdout, stderr, reason, exit_code)
-        if string.len(stdout) > 0 then
-            text:set_text("On")
-        else
-            text:set_text("Off")
-        end
-    end,
-    spotify_widget
-)
