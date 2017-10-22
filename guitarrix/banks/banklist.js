90bd4f85d7df10f255b6d060df89a1362f067b44
Francesc Arpí <francesc.arpi@gmail.com>
2017-10-22 19:11:23 +0200

    Reset

--- guitarrix/banks/banklist.js
+++
@@ -1,6 +0,0 @@
-[
-  ["nuclear", "nuclear.gx", 1, 0, [1, 2], 1485190846],
-  ["mkenny_17tone", "mkenny_17tone.gx", 1, 0, [1, 2], 1485190737],
-  ["Cesc", "Cesc.gx", 1, 0, [1, 2], 1485192064],
-  ["Scratchpad", "scratchpad.gx", 0, 0, [1, 2], 1485190678]
-]
--- xinitrc.d/20-graphics.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --setprovideroffloadsink "OLAND @ pci:0000:05:00.0" Intel
