b75ae5ccd9a8826c37aec6a0b2563a43a8fffeea
Francesc Arpi <francesc.arpi@gmail.com>
2018-02-26 19:20:57 +0100

    Backup

--- i3/status.py
+++ i3/status.py
@@ -5,59 +5,104 @@
 
 from i3pystatus import Status, get_module
 from i3pystatus.core.command import run_through_shell
+from i3pystatus.updates import pacman, cower
 
 from subprocess import Popen, PIPE
 
-status = Status(standalone=True)
+status = Status()
 
-# main clock
-status.register("clock", format="%d/%m/%Y %H:%M",)
+status.register("updates",
+    format = "Updates: {count}",
+    format_no_updates = "",
+    backends = [pacman.Pacman(), cower.Cower()])
 
-# load average
-status.register("load")
 
-# audio
-status.register("pulseaudio", format="V {volume}% {muted}",)
+status.register("clock",
+    format=" %H:%M:%S",
+    color='#C678DD',
+    interval=1,
+    on_leftclick="/usr/bin/gsimplecal",)
 
-# backlight
-@get_module
-def change_brightness(self):
-    process = Popen(["xbacklight"], stdout=PIPE)
-    output, error = process.communicate()
-    process.wait()
-    current = round(float(output[:-1]))
 
-    if current == 100.0:
-        setback = 15
-    elif current == 15.0:
-        setback = 30
-    elif current == 30.0:
-        setback = 50
-    elif current == 50:
-        setback = 75
-    else:
-        setback = 100
+status.register("pulseaudio",
+    color_unmuted='#98C379',
+    color_muted='#E06C75',
+    format_muted=' [muted]',
+    format=" {volume}%")
 
-    run_through_shell(["xbacklight", "-set", str(setback)])
+
+status.register("network",
+    interface="enp0s25",
+    color_up="#8AE234",
+    color_down="#EF2929",
+    format_up=": {v4cidr}",
+    format_down="",)
+
+
+status.register("network",
+    interface="wlp4s0",
+    color_up="#8AE234",
+    color_down="#EF2929",
+    format_up="  {essid}  {kbs} kbs",
+    format_down="",)
+
+
+status.register("backlight",
+    interval=5,
+    format=" {percentage:.0f}%",
+    backlight="intel_backlight",)
+
+
+status.register("battery",
+    battery_ident="BAT0",
+    interval=5,
+    format="{status} {percentage:.0f}%",
+    alert=True,
+    alert_percentage=15,
+    color="#FFFFFF",
+    critical_color="#FF1919",
+    charging_color="#E5E500",
+    full_color="#D19A66",
+    status={
+        "DIS": " ",
+        "CHR": "  ",
+        "FULL": "   ",
+},)
+
+status.register("temp",
+    color='#78EAF2',
+                )
+
+status.register("mem",
+    color="#999999",
+    warn_color="#E5E500",
+    alert_color="#FF1919",
+    format=" {avail_mem}/{total_mem} GB",
+    divisor=1073741824,)
+
+status.register("disk",
+    color='#56B6C2',
+    path="/home",
+    on_leftclick="pcmanfm",
+    format=" {avail} GB",)
+
+
+status.register("keyboard_locks",
+    format='{caps}',
+    caps_on='Caps Lock',
+    caps_off='',
+    color='#e60053',
+    )
+
 
 status.register(
-    "backlight",
-    format="B {percentage:.0f}%",
-    backlight="intel_backlight",
-    on_leftclick=[change_brightness]
+    "spotify",
+    format=" {status} {length} {artist} - {title}",
+    format_not_running=" Not running"
 )
 
-# battery
-status.register(
-    "battery",
-    format=("{status} [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}"),
-    path="/sys/class/power_supply/BAT0/uevent",
-    alert=False,
-    alert_percentage=10,
-    status={"DIS": "↓", "CHR": "↑", "FULL": "="}
-)
-
-status.register("spotify")
-status.register("mem", divisor=1024 ** 3, format="M {avail_mem} Gb")
+status.register("cpu_usage",
+    on_leftclick="termite --title=htop -e 'htop'",
+    format=" {usage}%",)
 
 status.run()
