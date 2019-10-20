85ee0809d4ef44e5775a59a23fb637c4a9dca518
Francesc Arpí <francesc.arpi@gmail.com>
2019-10-20 11:06:07 +0200

    Backup

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -259,6 +259,9 @@ globalkeys = gears.table.join(
     awful.key({modkey,}, "j", function () awful.client.focus.byidx(-1) end,
               {description = "focus next by index", group = "client"}),
 
+    awful.key({modkey,}, "Tab", function () awful.client.focus.byidx(1) end,
+              {description = "focus next by index", group = "client"}),
+
     awful.key({modkey,}, "k", function () awful.client.focus.byidx(1) end,
               {description = "focus previous by index", group = "client"}),
 
@@ -280,7 +283,7 @@ globalkeys = gears.table.join(
     awful.key({modkey,}, "u", awful.client.urgent.jumpto,
               {description = "jump to urgent client", group = "client"}),
 
-    awful.key({modkey,}, "Tab",
+    awful.key({modkey,}, "º",
         function ()
             awful.client.focus.history.previous()
             if client.focus then
