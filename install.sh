45a2684a0fc3d0a57e9ab29c995b33165de0056d
Francesc Arpi Roca <francesc.arpi@gmail.com>
2021-11-01 20:16:12 +0100

    Readme, install and p10k improvements
    Readme updates, removing brew at the moment. Also the "cleaning" block
    has been removed from the installer script. Finally, some unused plugins
    disabled in the p10k shell.

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Cleaning previous symlinks
-rm $HOME/.tmux.conf
-rm $HOME/.zshrc
-rm $HOME/.ideavimrc
-rm -Rf $HOME/.config/lvim
-rm -Rf $HOME/.config/zsh
-
 # Symlinks
+echo "Installing symlinks..."
 stow -t $HOME */
 
 # Installing required zsh modules
-git submodule update --init --recursive "zsh/.config/zsh/modules/z"
-git submodule update --init --recursive "zsh/.config/zsh/modules/zsh-completions"
-git submodule update --init --recursive "zsh/.config/zsh/modules/zsh-autosuggestions"
+if [ $? -eq 0 ]; then
+  echo "Pulling submodules..."
+  git submodule update --init --recursive "zsh/.config/zsh/modules/z"
+  git submodule update --init --recursive "zsh/.config/zsh/modules/zsh-completions"
+  git submodule update --init --recursive "zsh/.config/zsh/modules/zsh-autosuggestions"
+fi
+
+echo "Done!"
 
