ce0ce952240b46a3c1ef5b5862ca1aa7507688b8
Francesc Arpi Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2021-11-02 13:21:59 +0100

    Lvim theme changed to gruvbox

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "onedarker"
+lvim.colorscheme = "gruvbox8_hard"
 
 lvim.lsp.diagnostics.virtual_text = false
 
