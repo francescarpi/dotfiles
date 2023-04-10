41f67ffb3f17302550185b51ea3b21173ab37729
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-04-10 09:32:46 +0200

    update lualine

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -83,10 +83,10 @@ require("Comment").setup()
 
 require("lualine").setup({
   sections = {
-    lualine_a = {},
+    lualine_a = { "mode" },
     lualine_b = { "branch", "diff", "diagnostics" },
     lualine_c = { { "filename", path = 1 } },
-    lualine_x = {},
+    lualine_x = { 'filetype' },
     lualine_y = { "progress" },
     lualine_z = { "location" },
   },
