07f69eb7349777fc736e9bb48dbf0241eff31a40
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2023-04-12 14:56:45 +0200

    wezterm keys ordered

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -19,43 +19,16 @@ config.send_composed_key_when_right_alt_is_pressed = true
 config.send_composed_key_when_left_alt_is_pressed = true
 
 config.keys = {
-  {
-    key = 'i',
-    mods = 'CMD',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'u',
-    mods = 'CMD',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'z',
-    mods = 'CMD',
-    action = wezterm.action.TogglePaneZoomState,
-  },
-  {
-    key = 'h',
-    mods = 'CMD',
-    action = act.ActivatePaneDirection 'Left',
-  },
-  {
-    key = 'l',
-    mods = 'CMD',
-    action = act.ActivatePaneDirection 'Right',
-  },
-  {
-    key = 'k',
-    mods = 'CMD',
-    action = act.ActivatePaneDirection 'Up',
-  },
-  {
-    key = 'j',
-    mods = 'CMD',
-    action = act.ActivatePaneDirection 'Down',
-  },
+  { key = 'i', mods = 'CMD', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { key = 'u', mods = 'CMD', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+  { key = 'z', mods = 'CMD', action = wezterm.action.TogglePaneZoomState },
+  { key = 'h', mods = 'CMD', action = act.ActivatePaneDirection 'Left' },
+  { key = 'l', mods = 'CMD', action = act.ActivatePaneDirection 'Right' },
+  { key = 'k', mods = 'CMD', action = act.ActivatePaneDirection 'Up' },
+  { key = 'j', mods = 'CMD',action = act.ActivatePaneDirection 'Down' },
   { key = 'LeftArrow', mods = 'CMD', action = act.ActivateTabRelative(-1) },
   { key = 'RightArrow', mods = 'CMD', action = act.ActivateTabRelative(1) },
+  { key = 'o', mods = 'CMD',action = act.ActivateLastTab },
 }
 
 -- and finally, return the configuration to wezterm
