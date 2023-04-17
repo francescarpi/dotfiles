1f2260cd5d50fcabacf4093fec71e658ed8548d4
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2023-04-17 13:43:09 +0200

    adding plugins (wip)

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,3 +1,7 @@
+-- -----------------------------------------------------------------------------------------------
+-- Plugins
+-- -----------------------------------------------------------------------------------------------
+
 -- Install package manager
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
@@ -12,3 +16,52 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
+
+-- Setup
+require('lazy').setup({
+  -- ------------------------------------------------------------------------------------------
+  {
+    "tanvirtin/monokai.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme monokai_pro]])
+    end,
+  },
+
+  -- ------------------------------------------------------------------------------------------
+  {"kyazdani42/nvim-web-devicons"},
+
+  -- ------------------------------------------------------------------------------------------
+  {"lukas-reineke/indent-blankline.nvim", opts = {} },
+
+  -- ------------------------------------------------------------------------------------------
+  {"numToStr/Comment.nvim", opts = {} },
+
+  -- ------------------------------------------------------------------------------------------
+  {"nvim-lualine/lualine.nvim", opts = {
+      options = {
+        disabled_filetypes = {
+          statusline = { 'NvimTree' }
+      }
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = { { "filename", path = 1 } },
+      lualine_x = { 'filetype' },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+      },
+    }
+  },
+
+  -- ------------------------------------------------------------------------------------------
+  {"nvim-tree/nvim-tree.lua", opts = {
+      hijack_cursor = false
+    }
+  },
+
+})
+
+
