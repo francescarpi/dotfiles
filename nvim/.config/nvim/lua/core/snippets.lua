c37e66b372043b2eab6af77d94f45fb74462a26d
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-04-24 19:55:53 +0200

    snippet simplified

--- nvim/.config/nvim/lua/core/snippets.lua
+++ nvim/.config/nvim/lua/core/snippets.lua
@@ -3,7 +3,5 @@ local s = ls.snippet
 local t = ls.text_node
 
 ls.add_snippets("python", {
-  s({ trig = "ipdb", dscr = "Ipdb set_trace" }, {
-    t("import ipdb; ipdb.set_trace()"),
-  }),
+  s("ipdb", t"import ipdb; ipdb.set_trace()" ),
 })
