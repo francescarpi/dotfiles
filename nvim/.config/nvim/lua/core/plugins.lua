fbfd9b85c9025fa7d0cf481717ceda69485aa30f
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2023-04-24 14:37:33 +0200

    snippets configured

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -145,7 +145,7 @@ require('lazy').setup({
   -- ------------------------------------------------------------------------------------------
   { "hrsh7th/nvim-cmp" },
   { "hrsh7th/cmp-nvim-lsp" },
-  { "L3MON4D3/LuaSnip" },
+  { "L3MON4D3/LuaSnip", opts = {} },
   { "hrsh7th/cmp-buffer" },
   { "hrsh7th/cmp-path" },
 
---
+++ nvim/.config/nvim/lua/core/snippets.lua
@@ -0,0 +1,10 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+
+ls.add_snippets("python", {
+  s("ipdb", {
+    t("import ipdb; ipdb.set_trace()"),
+  })
+})
+
