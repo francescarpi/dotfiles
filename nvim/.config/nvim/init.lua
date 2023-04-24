feb514bb990c735192d36221462407f50a542208
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2023-04-24 14:57:42 +0200

    leader changed to space

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,8 +5,8 @@
 -- Set leader key
 -- See: `:help mapleader`
 -- NOTE: Must happen before. Plugins are required
-vim.g.mapleader = ','
-vim.g.maplocalleader = ','
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 require("core.plugins")
 require("core.options")
