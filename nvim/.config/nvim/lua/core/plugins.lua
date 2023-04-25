c86b443a7daf758cf7a47dec3d0c59c84be128e8
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2023-04-25 11:08:47 +0200

    fixed diffview installation

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -3,29 +3,29 @@
 -- -----------------------------------------------------------------------------------------------
 
 -- Install package manager
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
     lazypath,
-  }
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup
-require('lazy').setup({
+require("lazy").setup({
   -- ------------------------------------------------------------------------------------------
   {
     "tanvirtin/monokai.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'monokai_pro'
+      vim.cmd.colorscheme("monokai_pro")
     end,
   },
 
@@ -44,18 +44,18 @@ require('lazy').setup({
     opts = {
       options = {
         disabled_filetypes = {
-          statusline = { 'NvimTree' }
-        }
+          statusline = { "NvimTree" },
+        },
       },
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = { { "filename", path = 1 } },
-        lualine_x = { 'filetype' },
+        lualine_x = { "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
       },
-    }
+    },
   },
 
   -- ------------------------------------------------------------------------------------------
@@ -64,9 +64,9 @@ require('lazy').setup({
     opts = {
       hijack_cursor = false,
       update_focused_file = {
-        enable = true
-      }
-    }
+        enable = true,
+      },
+    },
   },
 
   -- ------------------------------------------------------------------------------------------
@@ -78,16 +78,15 @@ require('lazy').setup({
         diffview = true,
       },
     },
-    dependencies = { "nvim-lua/plenary.nvim" },
-    requires = { "sindrets/diffview.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
   },
 
   -- ------------------------------------------------------------------------------------------
   {
     "lewis6991/gitsigns.nvim",
     opts = {
-      current_line_blame = true
-    }
+      current_line_blame = true,
+    },
   },
 
   -- ------------------------------------------------------------------------------------------
@@ -106,21 +105,21 @@ require('lazy').setup({
       { "folke/neodev.nvim",               opts = {} }, -- Additional lua configuration, makes nvim stuff amazing!
       { "jose-elias-alvarez/null-ls.nvim", opts = {} },
       "jay-babu/mason-null-ls.nvim",
-    }
+    },
   },
 
   -- ------------------------------------------------------------------------------------------
   {
     -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
     dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
+      "nvim-treesitter/nvim-treesitter-textobjects",
     },
     build = ":TSUpdate",
   },
 
   -- ------------------------------------------------------------------------------------------
-  { "ThePrimeagen/harpoon", opts = {}, dependencies = { "nvim-lua/plenary.nvim" } },
+  { "ThePrimeagen/harpoon",    opts = {}, dependencies = { "nvim-lua/plenary.nvim" } },
 
   -- ------------------------------------------------------------------------------------------
   {
@@ -131,12 +130,12 @@ require('lazy').setup({
         mappings = {
           i = {
             ["<C-j>"] = "move_selection_next",
-            ["<C-k>"] = "move_selection_previous"
-          }
-        }
-      }
+            ["<C-k>"] = "move_selection_previous",
+          },
+        },
+      },
     },
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim" },
   },
 
   -- ------------------------------------------------------------------------------------------
@@ -149,5 +148,4 @@ require('lazy').setup({
   { "saadparwaiz1/cmp_luasnip" },
   { "hrsh7th/cmp-buffer" },
   { "hrsh7th/cmp-path" },
-
 })
