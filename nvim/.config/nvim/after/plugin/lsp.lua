b9a02e40df8b3bf13ef5f6882083800d5c371dd8
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-04-26 18:24:24 +0200

    lsp improvements

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,23 +1,8 @@
--- node
-local home_dir = os.getenv("HOME")
-local node_version = "16.16.0"
-local node_bin = home_dir .. "/.nvm/versions/node/v" .. node_version .. "/bin"
+local lsp = require('lsp-zero').preset('recommended')
 
-if not vim.loop.fs_stat(node_bin .. "/node") then
-  error("Node version '" .. node_version .. "' is not present. Please, installe it.")
-else
-  vim.g.node_host_prog = node_bin .. "/node"
-  vim.cmd("let $PATH = '" .. node_bin .. ":' . $PATH")
-end
-
--- lsp
-local lsp = require('lsp-zero').preset({})
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+  lsp.default_keymaps({ buffer = bufnr })
 end)
-
--- languages
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.ensure_installed({
   'pyright',
@@ -30,5 +15,13 @@ lsp.ensure_installed({
   'volar',
   'lua_ls',
 })
+
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').ruff_lsp.setup({
+  init_options = {
+    settings = { args = { "--line-length", "120" }, organizeImports = true, fixAll = true }
+  }
+})
+
 
 lsp.setup()
