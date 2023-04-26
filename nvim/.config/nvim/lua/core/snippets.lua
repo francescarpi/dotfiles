302bed11f39fd7b172eebe796fb25142d9fcfa4c
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2023-04-26 11:56:47 +0200

    new spinnet

--- nvim/.config/nvim/lua/core/snippets.lua
+++ nvim/.config/nvim/lua/core/snippets.lua
@@ -1,7 +1,12 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
 
 ls.add_snippets("python", {
-  s("ipdb", t"import ipdb; ipdb.set_trace()" ),
+  s("ipdb", t("import ipdb; ipdb.set_trace()")),
+})
+
+ls.add_snippets("vue", {
+  s("log", { t("console.log("), i(1), t(");") }),
 })
