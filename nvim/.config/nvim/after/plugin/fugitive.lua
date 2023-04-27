7a5a35fc25ca350501de2069af9530a2700292e2
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2023-04-27 12:24:22 +0200

    refactor(nvim): delete custom command to exit from fugitive

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,8 +1,1 @@
 vim.keymap.set("n", "<leader>gg", ":G | only<CR>", { silent = true, desc = "Toggle git" })
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "fugitive",
-  callback = function(args)
-    vim.keymap.set("n", "q", ":bdelete<CR>", { silent = true, desc = "Quit from fugitive" })
-  end
-})
