5c491fda6bf997fee39e371e81e4d3b20fac2b2b
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-05-03 20:47:00 +0200

    feat(nvim): add tagbar

---
+++ nvim/.config/nvim/after/plugin/tagbar.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "<leader>t", ":TagbarToggle<CR>", { silent = true, desc = "Toggle Tagbar" })
--- nvim/.config/nvim/lua/core/packer.lua
+++ nvim/.config/nvim/lua/core/packer.lua
@@ -98,6 +98,9 @@ return require('packer').startup(function(use)
   -- --------------------------------------------------------------------------------------------
   use 'tpope/vim-fugitive'
 
+  -- --------------------------------------------------------------------------------------------
+  use 'preservim/tagbar'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
