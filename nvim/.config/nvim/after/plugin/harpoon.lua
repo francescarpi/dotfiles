c1daa38bcca2aa3495c19dcadb443c501ad3546e
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-05-03 20:49:06 +0200

    feat(nvim): add descriptions to harpoon key bindings

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -3,10 +3,10 @@ local ui = require("harpoon.ui")
 
 require('harpoon').setup {}
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add file to harpoon" })
+vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, { desc = "Toggle harpoon popup" })
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end, { desc = "Nav to harpoon file number 1" })
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(2) end, { desc = "Nav to harpoon file number 2" })
+vim.keymap.set("n", "<C-t>", function() ui.nav_file(3) end, { desc = "Nav to harpoon file number 3" })
+vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end, { desc = "Nav to harpoon file number 4" })
