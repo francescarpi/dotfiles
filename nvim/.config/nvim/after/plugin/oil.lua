1565cd559ae0de49d49c4383e3e4578f7ece91be
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-06-13 14:34:51 +0200

    feat(nvim): oil show hidden files by default

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -1,4 +1,8 @@
-require("oil").setup()
+require("oil").setup({
+  view_options = {
+    show_hidden = true
+  }
+})
 
 -- vim.keymap.set("n", "<leader>v", ":NvimTreeToggle<CR>", { silent = true, desc = "Nvimtree: Toggle" })
 vim.keymap.set("n", "<leader>o", require("oil").open, { desc = "Open parent directory" })
