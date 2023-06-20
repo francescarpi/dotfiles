2e7b280d3e1f0a9bb5ea9b256d45c28cddeb694f
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-06-20 17:56:00 +0200

    feat(zsh): update workon function

--- zsh/.config/zsh/functions.sh
+++ zsh/.config/zsh/functions.sh
@@ -1,4 +1,5 @@
 workon() {
+
   # .venv fodler in current directory
   if [ -d ".venv" ]
   then
@@ -10,6 +11,13 @@ workon() {
   if [ -d "../.venv" ]
   then
     source ../.venv/bin/activate
+    return 0
+  fi
+
+  # venv fodler in current directory
+  if [ -d "venv" ]
+  then
+    source venv/bin/activate
     return 0
   fi
 
