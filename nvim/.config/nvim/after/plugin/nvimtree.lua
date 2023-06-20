0581fe3c5d1773b9265f1973b831607b9ac46c33
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-06-20 17:55:37 +0200

    feat(nvim): change some settings of nvim-tree

--- nvim/.config/nvim/after/plugin/nvimtree.lua
+++ nvim/.config/nvim/after/plugin/nvimtree.lua
@@ -1,8 +1,14 @@
 require('nvim-tree').setup {
   hijack_cursor = false,
-      update_focused_file = {
-        enable = true,
-      },
+  update_focused_file = {
+    enable = true,
+  },
+  filters = {
+    dotfiles = true
+  },
+  view = {
+    width = 55
+  }
 }
 
 vim.keymap.set("n", "<leader>v", ":NvimTreeToggle<CR>", { silent = true, desc = "Nvimtree: Toggle" })
