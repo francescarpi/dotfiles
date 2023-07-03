693fe024465dab69343c1c1a0481dc0fc293c5d8
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-07-03 16:26:35 +0200

    feat(nvim): remap for sneak

--- nvim/.config/nvim/after/plugin/sneak.lua
+++ nvim/.config/nvim/after/plugin/sneak.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "f", "<Plug>Sneak_s", { desc = "Sneak foreward" })
-vim.keymap.set("n", "F", "<Plug>Sneak_S", { desc = "Sneak backward" })
+vim.keymap.set("n", "<leader>s", "<Plug>Sneak_s", { desc = "Sneak foreward" })
+vim.keymap.set("n", "<leader>S", "<Plug>Sneak_S", { desc = "Sneak backward" })
