07a53d59795fd8587004f819491e0e6af9e6d3bf
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-07-16 08:57:17 +0200

    feat(nvim): force neogit colors

--- nvim/.config/nvim/after/plugin/neogit.lua
+++ nvim/.config/nvim/after/plugin/neogit.lua
@@ -1,4 +1,5 @@
 local neogit = require('neogit')
+
 neogit.setup {
   disable_commit_confirmation = true,
   disable_hint = true,
--- nvim/.config/nvim/after/plugin/theme.lua
+++ nvim/.config/nvim/after/plugin/theme.lua
@@ -1,11 +1,22 @@
 local monokai = require('monokai')
 
 vim.cmd [[colorscheme monokai]]
+
+------------------------------------------------------------------------------
+-- Neogit
+vim.api.nvim_set_hl(0, "NeogitStagedchanges", { fg = monokai.classic.aqua, bg = monokai.classic.base0 })        -- Unstaged changes
+vim.api.nvim_set_hl(0, "NeogitUnstagedchanges", { fg = monokai.classic.aqua, bg = monokai.classic.base0 })        -- Unstaged changes
+vim.api.nvim_set_hl(0, "NeogitRecentCommits", { fg = monokai.classic.aqua, bg = monokai.classic.base0 })          -- Recent commits
+vim.api.nvim_set_hl(0, "NeogitBranch", { fg = monokai.classic.yellow, bg = monokai.classic.base0 })               -- Head branch
+vim.api.nvim_set_hl(0, "NeogitRemote", { fg = monokai.classic.yellow, bg = monokai.classic.base0 })               -- Push branch
+
+vim.api.nvim_set_hl(0, "NeogitDiffAddHighlight", { fg = monokai.classic.green, bg = monokai.classic.base0 })      -- Added
+vim.api.nvim_set_hl(0, "NeogitDiffDeleteHighlight", { fg = monokai.classic.pink, bg = monokai.classic.base0 })    -- Removed
+
+vim.api.nvim_set_hl(0, "NeogitHunkHeader", { fg = monokai.classic.orange, bg = monokai.classic.base0 })           -- Header
+vim.api.nvim_set_hl(0, "NeogitHunkHeaderHighlight", { fg = monokai.classic.orange, bg = monokai.classic.base0 })  -- Header highlighted
+
+------------------------------------------------------------------------------
+-- tabs
 vim.cmd("highlight TabLineSel guibg=#ffaf26 guifg=#000000")
 
--- Neogit
-vim.api.nvim_set_hl(0, "NeogitDiffAddHighlight", { fg = monokai.classic.green, bg = monokai.classic.base0 })
-vim.api.nvim_set_hl(0, "NeogitDiffDeleteHighlight", { fg = monokai.classic.red, bg = monokai.classic.base0 })
-vim.api.nvim_set_hl(0, "NeogitDiffContextHighlight", { fg = "#b2b2b2", bg = monokai.classic.base0 })
-vim.api.nvim_set_hl(0, "NeogitHunkHeader", { fg = "#073642", bg = "#ffaf26" })
-vim.api.nvim_set_hl(0, "NeogitHunkHeaderHighlight", { fg = "#073642", bg = "#ffaf26" })
