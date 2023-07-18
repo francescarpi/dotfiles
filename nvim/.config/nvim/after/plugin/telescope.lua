508b640dad352aaf99fe4f028a0c1a9c18cc90c7
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2023-07-18 10:28:29 +0200

    feat(nvim): adds new keymap to use telescope's git status

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -33,3 +33,4 @@ vim.keymap.set("n", "<leader>fs", "<cmd>lua require'telescope.builtin'.live_grep
 vim.keymap.set("n", "<leader>fb", "<cmd>lua require'telescope.builtin'.buffers{}<CR>", { silent = true, desc = "Telescope: buffers" })
 vim.keymap.set("n", "<leader>fd", "<cmd>lua require'telescope.builtin'.diagnostics{}<CR>", { silent = true, desc = "Telescope: diagnostics" })
 vim.keymap.set("n", "<leader>fk", "<cmd>lua require'telescope.builtin'.keymaps{}<CR>", { silent = true, desc = "Telescope: keymaps" })
+vim.keymap.set("n", "<leader>fg", "<cmd>lua require'telescope.builtin'.git_status{}<CR>", { silent = true, desc = "Telescope: git_status" })
