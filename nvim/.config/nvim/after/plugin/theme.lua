f99c7b911565d8e25ae346ac42735b14ba42e803
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-07-18 21:15:01 +0200

    feat(nvim): catputchin theme removed

--- nvim/.config/nvim/after/plugin/theme.lua
+++ nvim/.config/nvim/after/plugin/theme.lua
@@ -1,15 +1,2 @@
--- require("catppuccin").setup({
---   integrations = {
---     cmp = true,
---     gitsigns = true,
---     nvimtree = true,
---     treesitter = true,
---     notify = true,
---     neogit = true,
---   }
--- })
---
--- vim.cmd.colorscheme "catppuccin-mocha"
-
 vim.cmd.colorscheme "monokai"
 vim.cmd("highlight TabLineSel guibg=#ffaf26 guifg=#000000")
