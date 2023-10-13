134956eb4d3a07833f104b0ed444363ecc121999
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2023-10-13 08:29:40 +0200

    feat(nvim): disable format on save

--- nvim/.config/nvim/lua/farpi/plugins/lsp/none-ls.lua
+++ nvim/.config/nvim/lua/farpi/plugins/lsp/none-ls.lua
@@ -1,15 +1,13 @@
 return {
 	"nvimtools/none-ls.nvim", -- configure formatters & linters
 	lazy = true,
-	-- event = { "BufReadPre", "BufNewFile" }, -- to enable uncomment this
+	event = { "BufReadPre", "BufNewFile" }, -- to enable uncomment this
 	dependencies = {
 		"jay-babu/mason-null-ls.nvim",
 	},
 	config = function()
 		local mason_null_ls = require("mason-null-ls")
-
 		local null_ls = require("null-ls")
-
 		local null_ls_utils = require("null-ls.utils")
 
 		mason_null_ls.setup({
@@ -27,9 +25,6 @@ return {
 		-- for conciseness
 		local formatting = null_ls.builtins.formatting -- to setup formatters
 		local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-
-		-- to setup format on save
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 		-- configure null_ls
 		null_ls.setup({
@@ -54,25 +49,6 @@ return {
 					end,
 				}),
 			},
-			-- configure format on save
-			on_attach = function(current_client, bufnr)
-				if current_client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({
-								filter = function(client)
-									--  only use null-ls for formatting instead of lsp server
-									return client.name == "null-ls"
-								end,
-								bufnr = bufnr,
-							})
-						end,
-					})
-				end
-			end,
 		})
 	end,
 }
