af895fc4d6ce9568b1e86d680417356029902853
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-10-15 08:57:50 +0200

    refactor(nvim): change some key mapping docs

--- nvim/.config/nvim/lua/farpi/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/farpi/plugins/bufferline.lua
@@ -15,8 +15,8 @@ return {
 			},
 		})
 
-		vim.keymap.set("n", "<leader>bl", ":BufferLinePick<CR>", { silent = true, desc = "Bufferline Pick" })
-		vim.keymap.set("n", "<leader>n", ":BufferLineCycleNext<CR>", { silent = true, desc = "Bufferline Next" })
-		vim.keymap.set("n", "<leader>p", ":BufferLineCyclePrev<CR>", { silent = true, desc = "Bufferline Previous" })
+		vim.keymap.set("n", "<leader>bl", ":BufferLinePick<CR>", { silent = true, desc = "Bufferline: Pick" })
+		vim.keymap.set("n", "<leader>n", ":BufferLineCycleNext<CR>", { silent = true, desc = "Bufferline: Next" })
+		vim.keymap.set("n", "<leader>p", ":BufferLineCyclePrev<CR>", { silent = true, desc = "Bufferline: Previous" })
 	end,
 }
