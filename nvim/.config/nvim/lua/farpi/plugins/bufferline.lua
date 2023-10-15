24a875259c8897ed164fc91ed33f081e5c78823d
Francesc Arpi Roca <francesc.arpi@gmail.com>
2023-10-15 08:16:44 +0200

    feat(nvim): change bufferline key mappings

--- nvim/.config/nvim/lua/farpi/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/farpi/plugins/bufferline.lua
@@ -16,7 +16,7 @@ return {
 		})
 
 		vim.keymap.set("n", "<leader>bl", ":BufferLinePick<CR>", { silent = true, desc = "Bufferline Pick" })
-		vim.keymap.set("n", "<leader>bn", ":BufferLineCycleNext<CR>", { silent = true, desc = "Bufferline Next" })
-		vim.keymap.set("n", "<leader>bp", ":BufferLineCyclePrev<CR>", { silent = true, desc = "Bufferline Previous" })
+		vim.keymap.set("n", "<leader>n", ":BufferLineCycleNext<CR>", { silent = true, desc = "Bufferline Next" })
+		vim.keymap.set("n", "<leader>p", ":BufferLineCyclePrev<CR>", { silent = true, desc = "Bufferline Previous" })
 	end,
 }
