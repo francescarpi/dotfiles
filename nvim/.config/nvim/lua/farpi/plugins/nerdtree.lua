e1aaa7d7f7e50cdfe3ab77f944dad37ffffcfa86
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2023-12-28 12:46:40 +0100

    feat(nvim): change nerdtree toggle by find

--- nvim/.config/nvim/lua/farpi/plugins/nerdtree.lua
+++ nvim/.config/nvim/lua/farpi/plugins/nerdtree.lua
@@ -1,6 +1,6 @@
 return {
 	"preservim/nerdtree",
 	config = function()
-		vim.keymap.set("n", "<leader><leader>", ":NERDTreeToggle<CR>", { silent = true, desc = "NERDTree: Toggle" })
+		vim.keymap.set("n", "<leader><leader>", ":NERDTreeFind<CR>", { silent = true, desc = "NERDTree: Toggle" })
 	end,
 }
