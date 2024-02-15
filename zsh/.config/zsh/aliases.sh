226bec1645d88348c37dc21dcd82e3068dd06f36
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2024-02-15 08:11:56 +0100

    feat(zsh): improve performance

--- zsh/.config/zsh/aliases.sh
+++ zsh/.config/zsh/aliases.sh
@@ -11,20 +11,6 @@ alias updatedb='sudo /usr/libexec/locate.updatedb'
 alias clean_pycache='find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf'
 alias current_week='date +%U'
 
-# GPG
-# https://www.howtogeek.com/427982/how-to-encrypt-and-decrypt-files-with-gpg-on-linux/
-alias gpg_new_key="gpg --full-generate-key"
-alias gpg_keys="gpg -k"
-alias gpg_export="gpg --armor --output $1 --export $2"
-alias gpg_import="gpg --import $1"
-alias gpg_decrypt="gpg --decrypt $1"
-
-# El flag 3 es opcional. Si no se pasa, se firma con la por defecto
-alias gpg_encrypt="gpg --encrypt --sign $3 -r $1 $2"
-
-# Virtualenvs
-alias cdvirtualenv="cd $VIRTUAL_ENV"
-
 # Wezterm
 alias tn="wezterm cli set-tab-title $1"
 
--- zsh/.config/zsh/functions.sh
+++ zsh/.config/zsh/functions.sh
@@ -40,3 +40,11 @@ clean_nvim_config() {
 }
 
 cx() { cd "$@" && l; }
+
+nvm() {
+  export NVM_DIR="$HOME/.nvm"
+  unset -f nvm
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  nvm "$@"
+}
