375caf26f240c8d16b1d62201bd4530d4619aba3
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-03-16 20:22:37 +0100

    feat(nvim): format code

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,22 +1,27 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    config = function()
-      require "configs.conform"
-    end,
-  },
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("configs.conform")
+		end,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      git = { enable = true },
-    },
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {
+			git = { enable = true },
+		},
+	},
 
-  {
-    "numToStr/Navigator.nvim",
-    config = function ()
-      require("Navigator").setup({})
-    end
-  }
+	{
+		"numToStr/Navigator.nvim",
+		config = function()
+			require("Navigator").setup({})
+			print("Navigator ready")
+			vim.keymap.set({ "n", "t" }, "<C-S-h>", "<CMD>NavigatorLeft<CR>")
+			vim.keymap.set({ "n", "t" }, "<C-S-l>", "<CMD>NavigatorRight<CR>")
+			vim.keymap.set({ "n", "t" }, "<C-S-k>", "<CMD>NavigatorUp<CR>")
+			vim.keymap.set({ "n", "t" }, "<C-S-j>", "<CMD>NavigatorDown<CR>")
+		end,
+	},
 }
