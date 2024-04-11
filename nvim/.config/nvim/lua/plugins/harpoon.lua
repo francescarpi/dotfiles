214a4f6cff668925834fcf87dd47a706c7b411db
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2024-04-11 08:35:05 +0200

    feat(nvim): change harpoon to version 2

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -3,56 +3,32 @@ local map = Util.safe_keymap_set
 
 return {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   config = function()
-    require("harpoon").setup({
-      menu = {
-        width = vim.api.nvim_win_get_width(0) - 100,
-      },
-    })
-    map(
-      "n",
-      "<leader>a",
-      "<cmd>lua require('harpoon.mark').add_file()<cr>",
-      { silent = true, desc = "Harpoon: Add file" }
-    )
-    map(
-      "n",
-      "<leader><tab>",
-      "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
-      { silent = true, desc = "Harpoon: Toggle quick menu" }
-    )
-    map(
-      "n",
-      "<leader>1",
-      "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",
-      { silent = true, desc = "Harpoon: Goto file 1" }
-    )
-    map(
-      "n",
-      "<leader>2",
-      "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",
-      { silent = true, desc = "Harpoon: Goto file 2" }
-    )
-    map(
-      "n",
-      "<leader>3",
-      "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",
-      { silent = true, desc = "Harpoon: Goto file 3" }
-    )
-    map(
-      "n",
-      "<leader>4",
-      "<cmd>lua require('harpoon.ui').nav_file(4)<cr>",
-      { silent = true, desc = "Harpoon: Goto file 4" }
-    )
-    map(
-      "n",
-      "<leader>5",
-      "<cmd>lua require('harpoon.ui').nav_file(5)<cr>",
-      { silent = true, desc = "Harpoon: Goto file 5" }
-    )
+    local harpoon = require("harpoon")
 
-    require("telescope").load_extension("harpoon")
-    map("n", "<leader>fh", "<cmd>:Telescope harpoon marks<CR>", { silent = true, desc = "Telescope: harpoon" })
+    harpoon:setup()
+
+    map("n", "<leader><tab>", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { silent = true, desc = "Harpoon: Toggle quick menu" })
+
+    map("n", "<leader>a", function()
+      harpoon:list():add()
+    end, { silent = true, desc = "Harpoon: Add file" })
+
+    map("n", "<C-i>", function()
+      harpoon:list():prev()
+    end, { silent = true, desc = "Harpoon: Jump to previous" })
+
+    map("n", "<C-o>", function()
+      harpoon:list():next()
+    end, { silent = true, desc = "Harpoon: Jump to next" })
+
+    for i = 1, 5, 1 do
+      map("n", "<leader>" .. i, function()
+        harpoon:list():select(i)
+      end, { silent = true, desc = "Harpoon: Jump to file with index " .. i })
+    end
   end,
 }
