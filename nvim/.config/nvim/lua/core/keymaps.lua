33950a7edf12fe41f10a5d4e184417b75ecdbf8b
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-04-16 12:09:09 +0200

    feat(nvim): add keymaps

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -8,3 +8,9 @@ vim.keymap.set("i", "jj", "<esc>", { desc = "Exit to normal mode" })
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- Switch faster between buffers
+vim.keymap.set("n", "<C-m>", "<cmd>e #<cr>", { desc = "Switch to other buffer" })
+
+-- Delete current buffer
+vim.keymap.set("n", "<leader>k", ":bdelete<CR>", { silent = true, desc = "Delete current buffer" })
