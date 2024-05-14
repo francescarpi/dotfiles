75f1d3579002e0c524436cb3e789c03aa84077eb
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-05-14 18:10:15 +0200

    feat(nvim): add sql plugin

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -107,6 +107,14 @@ return {
           { name = "path" },
         },
       })
+
+      cmp.setup.filetype({ "sql" }, {
+        sources = {
+          { name = "vim-dadbod-completion" },
+          { name = "buffer" },
+          { name = "copilot" },
+        },
+      })
     end,
   },
 
