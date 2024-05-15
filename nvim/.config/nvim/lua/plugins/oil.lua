a4515eb855878719091c8cd2953f9bec81bcd980
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-05-15 20:08:50 +0200

    feat(nvim): add some keybindings for oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,23 @@
 return {
   "stevearc/oil.nvim",
   config = function()
-    require("oil").setup()
-    vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { silent = true, desc = "Oil" })
+    require("oil").setup({
+      columns = { "icon" },
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ["<C-h>"] = false,
+        ["q"] = "actions.close",
+      },
+    })
+
+    vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { silent = true, desc = "Open parent directory" })
+    vim.keymap.set(
+      "n",
+      "<leader>-",
+      require("oil").toggle_float,
+      { silent = true, desc = "Open parent directory in floating window" }
+    )
   end,
 }
