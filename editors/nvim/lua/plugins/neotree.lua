a6b93a8cbc5d960c451f7471874d576ebad29dc0
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2024-06-07 10:24:45 +0200

    feat(nvim): move neotree in a separated file

---
+++ editors/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>e", "<CMD>Neotree toggle reveal<CR>", { desc = "Neotree toggle" })
+
+    require("neo-tree").setup({
+      close_if_last_window = true,
+    })
+  end,
+}
