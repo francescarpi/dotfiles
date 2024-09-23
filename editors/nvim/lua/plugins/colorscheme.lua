b82cfc0edd87eb8ecdc73674893c5a48ffcbf690
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2024-09-23 15:06:11 +0200

    feat(nvim): remove unused themes and define border color by mini files

--- editors/nvim/lua/plugins/colorscheme.lua
+++ editors/nvim/lua/plugins/colorscheme.lua
@@ -1,43 +1,11 @@
 return {
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   priority = 1000,
-  --   opts = {},
-  --   config = function()
-  --     vim.cmd([[colorscheme tokyonight]])
-  --   end,
-  -- },
-  -- {
-  --   "rose-pine/neovim",
-  --   name = "rose-pine",
-  --   priority = 1000,
-  --   opts = {},
-  --   config = function()
-  --     vim.cmd([[colorscheme rose-pine]])
-  --   end,
-  -- },
-  -- {
-  --   "catppuccin/nvim",
-  --   priority = 1000,
-  --   opvs = {},
-  --   config = function()
-  --     vim.cmd([[colorscheme catppuccin-mocha]])
-  --   end,
-  -- },
-  -- {
-  --   "ellisonleao/gruvbox.nvim",
-  --   priority = 1000,
-  --   opts = {},
-  --   config = function()
-  --     vim.cmd([[colorscheme gruvbox]])
-  --   end,
-  -- },
   {
     "dasupradyumna/midnight.nvim",
     priority = 1000,
     opts = {},
     config = function()
       vim.cmd([[colorscheme midnight]])
+      vim.api.nvim_set_hl(0, "MiniFilesBorder", { fg = "#ffffff" })
     end,
   },
 }
