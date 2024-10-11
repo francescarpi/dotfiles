5b947f21be62235c01d63f46dd93a12dcf244880
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-10-11 18:28:23 +0200

    feat(nvim): add catpuccin theme

--- editors/nvim/lua/plugins/colorscheme.lua
+++ editors/nvim/lua/plugins/colorscheme.lua
@@ -8,12 +8,22 @@ return {
   --     vim.api.nvim_set_hl(0, "MiniFilesBorder", { fg = "#ffffff" })
   --   end,
   -- },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function()
+  --     vim.cmd([[colorscheme tokyonight-night]])
+  --     vim.api.nvim_set_hl(0, "MiniFilesBorder", { fg = "#ffffff" })
+  --   end,
+  -- },
   {
-    "folke/tokyonight.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     opts = {},
     config = function()
-      vim.cmd([[colorscheme tokyonight-night]])
+      vim.cmd([[colorscheme catppuccin-mocha]])
       vim.api.nvim_set_hl(0, "MiniFilesBorder", { fg = "#ffffff" })
     end,
   },
