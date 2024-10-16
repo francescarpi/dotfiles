8401052dc2749bd9cf72efce90dc13ff52890150
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-10-16 20:49:45 +0200

    feat(nvim): remove commented code

--- editors/nvim/lua/plugins/colorscheme.lua
+++ editors/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,4 @@
 return {
-  -- {
-  --   "dasupradyumna/midnight.nvim",
-  --   priority = 1000,
-  --   opts = {},
-  --   config = function()
-  --     vim.cmd([[colorscheme midnight]])
-  --     vim.api.nvim_set_hl(0, "MiniFilesBorder", { fg = "#ffffff" })
-  --   end,
-  -- },
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   priority = 1000,
-  --   opts = {},
-  --   config = function()
-  --     vim.cmd([[colorscheme tokyonight-night]])
-  --     vim.api.nvim_set_hl(0, "MiniFilesBorder", { fg = "#ffffff" })
-  --   end,
-  -- },
   {
     "catppuccin/nvim",
     name = "catppuccin",
