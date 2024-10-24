8e4be1f5eb00734fdb9e27b1ddf139b8006d1217
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2024-10-24 12:23:56 +0200

    feat(nvim): add key binding to resume last search

--- editors/nvim/lua/plugins/telescope.lua
+++ editors/nvim/lua/plugins/telescope.lua
@@ -58,6 +58,7 @@ return {
       vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+      vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "Resume last search" })
       vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
       vim.keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
