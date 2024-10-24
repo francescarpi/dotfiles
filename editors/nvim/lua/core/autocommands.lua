e9b11f6cce12e408c3aeb559254cd7bd72ff1024
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-10-24 22:46:05 +0200

    feat(nvim): open qf file in another buffer

--- editors/nvim/lua/core/autocommands.lua
+++ editors/nvim/lua/core/autocommands.lua
@@ -27,6 +27,6 @@ vim.api.nvim_create_autocmd("BufEnter", {
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "qf",
   callback = function()
-    vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<C-w><Enter>", { noremap = true })
+    vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<C-w><Enter><C-w>T", { noremap = true })
   end,
 })
