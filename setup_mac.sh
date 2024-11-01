608ea08724440f34475e37ba5cb44f1592bdc0a0
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-11-01 17:09:01 +0100

    chore(setup-script): check and install system packages

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+echo "Setting symlink for dotfiles"
 files=$(find ./mac -type f -name "setup.sh" -perm -u=x)
-
 for file in $files; do
 (
     dir=$(dirname "$file")
@@ -9,5 +9,17 @@ for file in $files; do
     echo "Runing $file in $dir"
     ./setup.sh
 )
+done
+
+echo "Checking system packages"
+system_packages=(eza zoxide antidote atuin fnm starship)
+for pkg in "${system_packages[@]}"; do
+    echo -n "Checking $pkg..."
+    if brew ls --versions $pkg > /dev/null; then
+        echo " installed"
+    else
+        echo " not installed. Installing..."
+        brew install $pkg
+    fi
 done
 
