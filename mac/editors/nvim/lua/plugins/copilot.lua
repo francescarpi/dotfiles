1558cbae2a9a472baa776b649f3724f9c165ed78
Francesc Arpí Roca <francisco_de_asis_arpi_roca@external.mckinsey.com>
2024-11-11 09:44:04 +0100

    feat(nvim): remove bcp and keep only copilot

--- mac/editors/nvim/lua/plugins/copilot.lua
+++ mac/editors/nvim/lua/plugins/copilot.lua
@@ -1,20 +1,16 @@
 return {
-
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = { enabled = true, auto_trigger = true },
-      })
-    end,
-  },
-  {
-    "nikutsuki/bcp.nvim",
-    dependencies = { "zbirenbaum/copilot.lua" },
-    config = function()
-      require("bcp").setup()
-    end,
-  },
+  "zbirenbaum/copilot.lua",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-CR>",
+          prev = "<C-,>",
+          next = "<C-.>",
+        },
+      },
+    })
+  end,
 }
