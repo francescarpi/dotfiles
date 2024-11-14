7c864ea80a1bfd0e585aa0fc5c2f019ee4c5f6fb
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-11-14 19:33:04 +0100

    feat(nvim): grep key bindings adjustments

--- mac/editors/nvim/lua/plugins/grepper.lua
+++ mac/editors/nvim/lua/plugins/grepper.lua
@@ -1,12 +1,11 @@
 return {
   "mhinz/vim-grepper",
   config = function()
-    vim.keymap.set("n", "<Leader>gg", function()
+    vim.keymap.set("n", "<leader>gg", function()
       vim.cmd("Grepper")
     end, { silent = true })
     vim.g.grepper = {
       tools = { "rg", "git" },
     }
-    vim.keymap.set({ "n", "x" }, "gs", "<plug>(GrepperOperator)")
   end,
 }
