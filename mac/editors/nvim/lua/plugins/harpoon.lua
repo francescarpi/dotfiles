378a376f6b42d53349ecfb2a7aa8d804b79801b4
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-11-18 11:02:26 +0100

    feat(nvim): adjust some keys for harpoon

--- mac/editors/nvim/lua/plugins/harpoon.lua
+++ mac/editors/nvim/lua/plugins/harpoon.lua
@@ -10,20 +10,28 @@ return {
     end, { silent = true, desc = "Harpoon: Toggle quick menu" })
 
     vim.keymap.set("n", "<leader>a", function()
-      print("File added to Harpoon list")
+      print("File appended")
       harpoon:list():add()
-    end, { silent = true, desc = "Harpoon: Add file" })
+    end, { silent = true, desc = "Harpoon: Append file" })
 
-    vim.keymap.set("n", "<C-h>", function()
+    vim.keymap.set("n", "<leader>A", function()
+      print("File prepended")
+      harpoon:list():prepend()
+    end, { silent = true, desc = "Harpoon: Prepend file" })
+
+    vim.keymap.set("n", "<S-h>", function()
       harpoon:list():select(1)
     end, { silent = true, desc = "Harpoon: File 1" })
-    vim.keymap.set("n", "<C-j>", function()
+
+    vim.keymap.set("n", "<S-l>", function()
       harpoon:list():select(2)
     end, { silent = true, desc = "Harpoon: File 2" })
-    vim.keymap.set("n", "<C-k>", function()
+
+    vim.keymap.set("n", "<S-j>", function()
       harpoon:list():select(3)
     end, { silent = true, desc = "Harpoon: File 3" })
-    vim.keymap.set("n", "<C-l>", function()
+
+    vim.keymap.set("n", "<S-k>", function()
       harpoon:list():select(4)
     end, { silent = true, desc = "Harpoon: File 4" })
 
