ba696c6cecc373946c02f0d56a3068200f080b4e
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-11-22 18:16:25 +0100

    feat(nvim): replace theme by rose-pine

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,19 @@
 return {
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function()
+  --     vim.cmd([[colorscheme catppuccin-mocha]])
+  --     vim.api.nvim_set_hl(0, "MiniFilesBorder", { fg = "#ffffff" })
+  --   end,
+  -- },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {},
+    "rose-pine/neovim",
+    name = "rose-pine",
     config = function()
-      vim.cmd([[colorscheme catppuccin-mocha]])
+      vim.cmd([[colorscheme rose-pine]])
       vim.api.nvim_set_hl(0, "MiniFilesBorder", { fg = "#ffffff" })
     end,
   },
