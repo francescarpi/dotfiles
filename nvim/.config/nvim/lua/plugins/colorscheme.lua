56a91f219045b4d73b3128efd6fa7047602e3955
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-12-03 15:10:29 +0100

    feat(nvim): set catpuccing background transparent

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -12,6 +12,7 @@ return {
     config = function()
       require("catppuccin").setup({
         show_end_of_buffer = true,
+        transparent_background = true,
         integrations = {
           cmp = true,
           gitsigns = true,
