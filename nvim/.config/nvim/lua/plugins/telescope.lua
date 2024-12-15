09b17bc8df64cd7c971f1ef4e0f550e44caed9f8
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-12-15 19:07:47 +0100

    refactor(nvim): enable hidden files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,17 +1,18 @@
+local file_ignore_patterns = {
+  "node_modules",
+  ".git",
+}
+
 local register_key_bindings = function()
   local builtin = require("telescope.builtin")
+  local multigrep = require("plugins.telescope_plugins.multigrep")
 
   vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Search Keymaps", silent = true })
   vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Search Files", silent = true })
   vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "Resume last search", silent = true })
   vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Search Diagnostics", silent = true })
   vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Find existing buffers", silent = true })
-  vim.keymap.set(
-    "n",
-    "<leader>fg",
-    require("plugins.telescope_plugins.multigrep"),
-    { desc = "Grep allowing filter by file", silent = true }
-  )
+  vim.keymap.set("n", "<leader>fg", multigrep, { desc = "Grep allowing filter by file", silent = true })
 end
 
 return {
@@ -29,6 +30,9 @@ return {
 
       telescope.setup({
         pickers = {
+          find_files = {
+            hidden = true,
+          },
           buffers = {
             mappings = {
               i = {
@@ -38,9 +42,7 @@ return {
           },
         },
         defaults = {
-          file_ignore_patterns = {
-            "node_modules",
-          },
+          file_ignore_patterns = file_ignore_patterns,
           mappings = {
             i = {
               ["<esc>"] = actions.close,
