8e2a6d3f0b887b4a2f47d39ce408eb8530d64421
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-12-21 12:38:57 +0100

    refactor(nvim): ensure install more languages on treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,20 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     opts = {
-      ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+      ensure_installed = {
+        "bash",
+        "c",
+        "html",
+        "lua",
+        "luadoc",
+        "markdown",
+        "vim",
+        "vimdoc",
+        "dockerfile",
+        "javascript",
+        "python",
+        "rust",
+      },
       auto_install = true,
       highlight = {
         enable = true,
