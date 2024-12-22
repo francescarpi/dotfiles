756fb50c578aaec0c9594431d0c59f2b9b2960b1
Francesc Arpi Roca <francesc.arpi@gmail.com>
2024-12-22 07:31:09 +0100

    feat(yazi): add yazi configuration

--- zsh/.config/zsh/functions.sh
+++ zsh/.config/zsh/functions.sh
@@ -44,3 +44,11 @@ genuuid() {
   echo "UUID copied to clipboard"
 }
 
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
