8fd48d3948b7ca57a5f1a42bf097021992795da8
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-01-26 18:19:49 +0100

    refactor(nvim): configure buffon

--- nvim/.config/nvim/lua/plugins/buffon.lua
+++ nvim/.config/nvim/lua/plugins/buffon.lua
@@ -1,8 +1,14 @@
 return {
   {
+    -- "francescarpi/buffon.nvim",
     dir = "~/dev/neovim/buffon.nvim",
     opts = {
       cyclic_navigation = true,
+      open = { by_default = true },
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "nvim-lua/plenary.nvim",
     },
   },
 }
