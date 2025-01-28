9521915fc6378ec0f0910e5211450e977507d108
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-01-28 18:36:18 +0100

    refactor(nvim): simplify markdown configuration

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -2,23 +2,11 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {
-      file_types = { "Avante" },
       anti_conceal = {
-        enabled = true,
-        -- Which elements to always show, ignoring anti conceal behavior. Values can either be booleans
-        -- to fix the behavior or string lists representing modes where anti conceal behavior will be
-        -- ignored. Possible keys are:
-        --  head_icon, head_background, head_border, code_language, code_background, code_border
-        --  dash, bullet, check_icon, check_scope, quote, table_border, callout, link, sign
-        ignore = {
-          code_background = true,
-          sign = true,
-        },
+        enabled = false,
         above = 0,
         below = 0,
       },
