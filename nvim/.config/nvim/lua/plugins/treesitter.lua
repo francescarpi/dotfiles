f06ebf053276f20d5b7c82725034463c81a949ce
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-02-16 18:54:58 +0100

    feat(neovim): configure how to select and move classes

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -31,12 +31,14 @@ return {
           enable = true,
           goto_next_start = {
             ["]m"] = "@function.outer",
+            ["]]"] = "@class.outer",
           },
           goto_next_end = {
             ["]M"] = "@function.outer",
           },
           goto_previous_start = {
             ["[m"] = "@function.outer",
+            ["[["] = "@class.outer",
           },
           goto_previous_end = {
             ["[M"] = "@function.outer",
@@ -47,6 +49,8 @@ return {
           keymaps = {
             ["af"] = "@function.outer",
             ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
           },
         },
       },
