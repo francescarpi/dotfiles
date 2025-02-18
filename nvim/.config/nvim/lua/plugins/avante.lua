30dc6eed129d51397f87909c4bc6dbefcc9b07a8
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-02-18 19:03:44 +0100

    feat(nvim): configure avante with gemini

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -4,21 +4,12 @@ return {
     event = "VeryLazy",
     lazy = false,
     version = false,
+    enabled = true,
     opts = {
-      -- provider = "gemini",
-      provider = "copilot",
-      -- provider = "ollama",
-      vendors = {
-        gemini = {},
-        ollama = {
-          __inherited_from = "openai",
-          api_key_name = "",
-          endpoint = "http://127.0.0.1:11434/v1",
-          model = "codebooga",
-          -- model = "deepseek-coder-v2",
-          -- model = "deepseek-r1",
-          -- model = "codellama",
-        },
+      provider = "gemini",
+      vendors = {},
+      windows = {
+        position = "bottom",
       },
     },
     build = "make",
@@ -27,16 +18,6 @@ return {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons",
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-          enabled = false,
-        },
-        ft = { "markdown", "Avante" },
-      },
     },
   },
 }
