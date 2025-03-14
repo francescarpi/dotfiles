bd489897a9794d00d5c49f09713fc18b19fa562d
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-03-14 19:17:45 +0100

    refactor(neovim): copilot-chat adjustments

--- nvim/.config/nvim/lua/plugins/copilot-chat.lua
+++ nvim/.config/nvim/lua/plugins/copilot-chat.lua
@@ -1,16 +1,22 @@
 return {
   {
+    "github/copilot.vim",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.keymap.set("i", "<c-j>", 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+    end,
+  },
+  {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
       { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      window = {
-        layout = "float",
-      },
-    },
+    opts = {},
     keys = {
       {
         "<leader>o",
