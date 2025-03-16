c70ae2cc0a45ae23105c800603fbd2a41164c774
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-03-16 19:38:28 +0100

    feat(nvim): add diffview

--- nvim/.config/nvim/lua/plugins/telekasten.lua
+++ nvim/.config/nvim/lua/plugins/telekasten.lua
@@ -9,6 +9,7 @@ return {
       require("telekasten").setup({
         home = vim.fn.expand("~/.telekasten"),
       })
+
       vim.keymap.set(
         "n",
         "<leader>j",
@@ -16,12 +17,14 @@ return {
         { silent = true, desc = "Telekasten: Goto today" }
       )
       vim.keymap.set("n", "<leader>jj", "<cmd>Telekasten panel<cr>", { silent = true, desc = "Telekasten: Panel" })
+
       vim.keymap.set(
         "n",
         "<leader>jt",
         "<cmd>Telekasten toggle_todo<cr>",
         { silent = true, desc = "Telekasten: Toggle Todo" }
       )
+
       vim.api.nvim_set_hl(0, "tkTag", { fg = "gray" })
     end,
   },
