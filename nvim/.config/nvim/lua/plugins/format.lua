b5e772a733cef594ad563fb25b59f0180ef30a19
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-03-21 11:07:33 +0100

    fix(nvim): fix python formatting replacing ruff by ruff_format

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -23,7 +23,7 @@ return {
       end,
       formatters_by_ft = {
         lua = { "stylua" },
-        python = { "ruff" },
+        python = { "ruff_format" },
         javascript = { "prettierd", "prettier" },
         typescript = { "prettierd", "prettier" },
         typescriptreact = { "prettierd", "prettier" },
