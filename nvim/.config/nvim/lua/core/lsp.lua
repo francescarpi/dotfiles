6a753f4779e9f0cb2f28a1735fea245c74b9f2f0
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-04-08 20:36:05 +0200

    feat(nvim): add css lsp

--- nvim/.config/nvim/lua/core/lsp.lua
+++ nvim/.config/nvim/lua/core/lsp.lua
@@ -151,6 +151,17 @@ local servers = {
       },
     },
   },
+  css = {
+    cmd = { "vscode-css-language-server", "--stdio" },
+    filetypes = { "css", "scss", "less" },
+    init_options = { provideFormatter = true },
+    single_file_support = true,
+    settings = {
+      css = { validate = true },
+      scss = { validate = true },
+      less = { validate = true },
+    },
+  },
 }
 
 for name, config in pairs(servers) do
