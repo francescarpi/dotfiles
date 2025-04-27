4b001b2f61b35b0d82b790dc45375df0633e067e
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-04-27 18:17:17 +0200

    refactor(nvim): increase config of code companion

--- nvim/.config/nvim/lua/plugins/code-companion.lua
+++ nvim/.config/nvim/lua/plugins/code-companion.lua
@@ -1,10 +1,41 @@
 return {
   {
     "olimorris/codecompanion.nvim",
-    opts = {},
+    opts = {
+      display = {
+        diff = {
+          enabled = true,
+          close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
+          layout = "vertical", -- vertical|horizontal split for default provider
+          opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
+          provider = "default", -- default|mini_diff
+        },
+        chat = {
+          auto_scroll = false,
+          start_in_insert_mode = false, -- Open the chat buffer in insert mode?
+        },
+      },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      {
+        "Davidyz/VectorCode",
+        version = "*",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        cmd = "VectorCode",
+        build = "pipx upgrade vectorcode",
+      },
+      {
+        "OXY2DEV/markview.nvim",
+        lazy = false,
+        opts = {
+          preview = {
+            filetypes = { "markdown", "codecompanion" },
+            ignore_buftypes = {},
+          },
+        },
+      },
     },
     keys = {
       {
