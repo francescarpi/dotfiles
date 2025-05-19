0dd72814ea478488d8b105abb0d4428023e97a3c
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-05-19 17:07:28 +0200

    refactor(nvim): some buffon config tests

--- nvim/.config/nvim/lua/plugins/buffon.lua
+++ nvim/.config/nvim/lua/plugins/buffon.lua
@@ -4,11 +4,15 @@ return {
     dir = "~/dev/neovim/buffon.nvim",
     ---@type BuffonConfig
     opts = {
+      -- sort_buffers_by_loaded_status = true,
       -- num_pages = 1,
       -- keybindings = {
       --   goto_next_buffer = "false",
       --   goto_previous_buffer = "false",
       -- },
+      -- open = {
+      --   default_position = "bottom_right",
+      -- }
     },
     dependencies = {
       "nvim-tree/nvim-web-devicons",
