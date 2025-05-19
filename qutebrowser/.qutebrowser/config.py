a9954f79df2a31885c2d1da20d492545b85360e1
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-05-19 17:07:16 +0200

    feat(qutebrowser): add config

---
+++ qutebrowser/.qutebrowser/config.py
@@ -0,0 +1,10 @@
+# pylint: disable=C0111
+c = c  # noqa: F821 pylint: disable=E0602,C0103
+config = config  # noqa: F821 pylint: disable=E0602,C0103
+# pylint settings included to disable linting errors
+
+config.load_autoconfig()
+
+# c.auto_save.session = True # save tabs on quit/restart
+# c.content.notifications.enabled = True
+
