96f6cc450e64ee65aebeb08d7a8442b21516f06d
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-06-27 08:11:34 +0200

    refactor(nvim): disable auto brackets
    Finally I found the solution of the anoying problem with blink, that it
    added brackets after accept a completion.

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -4,6 +4,13 @@ return {
     version = "v0.*",
     opts = {
       keymap = { preset = "default" },
+      completion = {
+        accept = {
+          auto_brackets = {
+            enabled = false
+          }
+        }
+      },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = "mono",
