94735794235d7e96ed3b9ca7a8e2b613ce216c1c
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-09-08 07:40:01 +0200

    refactor: enable auto format on save in the conform plugin
    In addition, allows to disable it if .noformat files is added into the
    project root folder

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -1,9 +1,37 @@
+local uv = vim.uv or vim.loop
+
 local formatters = {
   json = { "jq" },
   lua = { "stylua" },
   typescriptreact = { "prettier" },
   typescript = { "prettier" },
 }
+
+local function find_in_ancestors(filename, startpath)
+  local dir = uv.fs_realpath(startpath or vim.fn.getcwd())
+  while dir do
+    local candidate = dir .. "/" .. filename
+    local stat = uv.fs_stat(candidate)
+    if stat then
+      return candidate
+    end
+    local parent = dir:match("(.+)/[^/]+$")
+    if parent == dir then break end
+    dir = parent
+  end
+  return nil
+end
+
+local function should_format(bufnr)
+  local bufname = vim.api.nvim_buf_get_name(bufnr)
+  if bufname == "" then return true end
+
+  if find_in_ancestors(".noformat", bufname) then
+    return false
+  end
+
+  return true
+end
 
 return {
   {
@@ -22,6 +50,13 @@ return {
     opts = {
       notify_on_error = false,
       formatters_by_ft = formatters,
+      format_on_save = function(bufnr)
+        if should_format(bufnr) then
+          return { timeout_ms = 500, lsp_fallback = true }
+        else
+          return nil
+        end
+      end,
       init = function()
         -- formatexpr is used by vim when formatting code blocks usigg gq
         vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
