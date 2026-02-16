f5b0b78e42629b6a84326c14048912c97b72005d
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-02-16 19:25:18 +0100

    feat(nvim): add deepseek avante config

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -1,6 +1,7 @@
 return {
   {
     "yetone/avante.nvim",
+    -- dir = "~/dev/avante.nvim",
     build = "make",
     event = "VeryLazy",
     version = false,
@@ -9,9 +10,22 @@ return {
     opts = {
       -- add any opts here
       -- this file can contain specific instructions for your project
-      instructions_file = "avante.md",
-      provider = "copilot",
-      providers = {},
+      -- instructions_file = "avante.md",
+      -- provider = "copilot",
+      provider = "deepseek",
+      providers = {
+        deepseek = {
+          __inherited_from = "openai",
+          api_key_name = "DEEPSEEK_API_KEY",
+          endpoint = "http://127.0.0.1:1234/v1",
+          model = "deepseek/deepseek-r1-0528-qwen3-8b",
+          disable_tools = true,
+          extra_request_body = {
+            temperature = 0,
+            -- max_tokens = 4096,
+          }
+        },
+      },
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
