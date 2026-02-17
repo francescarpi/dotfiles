d4a974cfc1dada23d5756b9d42fb197f90049d22
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-02-17 14:53:07 +0100

    refactor(nvim): remove llm model

--- nvim/.config/nvim/lua/plugins/llm.lua
+++ nvim/.config/nvim/lua/plugins/llm.lua
@@ -3,7 +3,6 @@ return {
     "huggingface/llm.nvim",
     opts = {
       backend = "openai",
-      model = "qwen/qwen3-coder-30b",
       url = "http://localhost:1234/v1",
       accept_keymap = "<c-j>",
     },
