2a349bb6d8fb14f5d421bc951231904b781831a0
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-02-18 15:41:02 +0100

    revert(nvim): add copilot again

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "github/copilot.vim",
+    opts = {},
+    dependencies = {},
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.keymap.set("i", "<c-j>", 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+    end,
+  },
+}
