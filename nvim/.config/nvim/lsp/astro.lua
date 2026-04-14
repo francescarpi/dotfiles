8747294a4eba433e6342bfb408e7db35bda93a93
Francesc Arpi Roca <francesc.arpi@gmail.com>
2026-04-14 15:23:31 +0200

    refactor(nvim): astro lsp format

--- nvim/.config/nvim/lsp/astro.lua
+++ nvim/.config/nvim/lsp/astro.lua
@@ -1,13 +1,13 @@
 local function get_typescript_server_path(root_dir)
-  local project_roots = vim.fs.find('node_modules', { path = root_dir, upward = true, limit = math.huge })
+  local project_roots = vim.fs.find("node_modules", { path = root_dir, upward = true, limit = math.huge })
   for _, project_root in ipairs(project_roots) do
-    local typescript_path = project_root .. '/typescript'
+    local typescript_path = project_root .. "/typescript"
     local stat = vim.loop.fs_stat(typescript_path)
-    if stat and stat.type == 'directory' then
-      return typescript_path .. '/lib'
+    if stat and stat.type == "directory" then
+      return typescript_path .. "/lib"
     end
   end
-  return ''
+  return ""
 end
 
 return {
